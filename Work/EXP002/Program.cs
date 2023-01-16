a0aaba6d0837f0b06b8603a6bf22f7de89b8308d
Evgeniya <evgeniya120@yandex.ru>
2023-01-16 21:12:44 +0300

    Решение 2-ой задачи

--- Work/EXP002/Program.cs
+++ Work/EXP002/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a, b, c;
+        
+        a = 2;
+        b = 3;
+        c = 7;
+    
+        
+        int max = a;
+        if (a > b)
+        {
+            a = max;
+            if (a > c)
+            {
+                a = max;
+            }
+            else
+            {
+                c = max;
+            }
+        }
+        else
+        {
+            b = max;
+            if (b > c)
+            {
+                b = max;
+            }
+            else
+            {
+                c = max;
+            }
+        }
+
+        Console.WriteLine(max);
+
