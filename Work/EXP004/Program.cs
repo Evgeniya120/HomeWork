0330a87fd7d3c090689b26711c39764bd5cb8406
Evgeniya <evgeniya120@yandex.ru>
2023-01-16 22:12:06 +0300

    Решение 4-ой задачи + диаграмма

---
+++ Work/EXP004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int[] array = { 1, 20, 3, 18, 5, 10, 43, 12 };
+int N = array.Length;
+int index = 0;
+
+while (N > index)
+{
+    if (array[index] % 2 == 0)
+
+        Console.WriteLine(array[index]);
+
+    index++;
+}
