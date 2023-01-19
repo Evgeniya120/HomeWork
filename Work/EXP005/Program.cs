c3d6b42bba2e70cdb5d58d8603cf6b4a05a468f6
Evgeniya <evgeniya120@yandex.ru>
2023-01-20 00:19:36 +0300

    Решение 1-ой задачи 2-ого семинара

--- Work/EXP005/Program.cs
+++ Work/EXP005/Program.cs
@@ -3,3 +3,16 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (100 <= a & a <= 999)
+{
+  a /= 10;
+  a = a % 10;
+  Console.WriteLine(a);
+}
+else
+{
+Console.WriteLine("Введите трехзначное число");
+}
