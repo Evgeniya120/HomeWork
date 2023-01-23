285ea08945abacc0c3f8e89e68d4445c478a7806
Evgeniya <evgeniya120@yandex.ru>
2023-01-23 10:46:02 +0300

    Решение 1-ой задачи 3 го семинара

--- Work/EXP008/Program.cs
+++ Work/EXP008/Program.cs
@@ -6,3 +6,19 @@
 
 // 23432 -> да
 // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ СТРОКИ
+
+
+bool Polindrom(int number1)
+
+{
+    int number2 = 0;
+    int x = number1;
+    while (number1 > 0)
+    {
+        int y = number1 % 10;
+        number1 = number1 / 10;
+        number2 = number2 * 10 + y;
+    }
+    return x == number2;
+}
+Console.WriteLine(Polindrom(23432));
